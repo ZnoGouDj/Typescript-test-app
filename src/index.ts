a8f69fcde8be870b11a7318219fae46ae7dfb787
Егор <znogoud@MacBook-Air-Egor.local>
2021-11-25 16:46:37 +0300

    Navigation implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import App from './pages/app';
+import './index.css';
 
 const app = new App();
 app.run();
--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,17 +1,59 @@
+import Header from '../../core/components/header';
+import Page from '../../core/templates/page';
 import MainPage from '../main';
+import SettingsPage from '../settings';
+import StatisticsPage from '../statistics';
+
+export const enum PageIds {
+  MainPage = 'main-page',
+  SettingsPage = 'settings-page',
+  StatisticsPage = 'statistics-page',
+}
 
 class App {
-  private container: HTMLElement;
+  private static container: HTMLElement = document.body;
+  private static defaultPageId: string = 'current-page';
   private initialPage: MainPage;
+  private header: Header;
+
+  static renderNewPage(idPage: string) {
+    const currentPageHTML = document.querySelector(`#${this.defaultPageId}`);
+    if (currentPageHTML) {
+      currentPageHTML.remove();
+    }
+    let page: Page | null = null;
+
+    if (idPage === PageIds.MainPage) {
+      page = new MainPage(idPage);
+    } else if (idPage === PageIds.SettingsPage) {
+      page = new SettingsPage(idPage);
+    } else if (idPage === PageIds.StatisticsPage) {
+      page = new StatisticsPage(idPage);
+    }
+
+    if (page) {
+      const pageHTML = page.render();
+      pageHTML.id = App.defaultPageId;
+      App.container.append(pageHTML);
+    }
+  }
+
+  private enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      App.renderNewPage(hash);
+    });
+  }
 
   constructor() {
-    this.container = document.body;
     this.initialPage = new MainPage('main-page');
+    this.header = new Header('header', 'header-container');
   }
 
   run() {
-    const mainPageHTML = this.initialPage.render();
-    this.container.append(mainPageHTML);
+    App.container.append(this.header.render());
+    App.renderNewPage('settings-page');
+    this.enableRouteChange();
   }
 }
 
